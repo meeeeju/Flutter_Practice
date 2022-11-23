d753e7750ab6172edc0efe271b22f808f11c05aa
meeeeju <meeju06@gmail.com>
2022-11-23 14:19:45 +0900

    feat:Apply Center Widget with mainAxisAlignment options

--- lib/mains.dart
+++ lib/mains.dart
@@ -10,35 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //실질적으로 모든 위젯을 감싸고 있음
-      title: "Charactor card", //Text() widget 사용하지 않는 이유 : 그냥 앱을 통칭하는 이름이므로
+      title: "MyApp", //Text() widget 사용하지 않는 이유 : 그냥 앱을 통칭하는 이름이므로
       theme: ThemeData(primarySwatch: Colors.blue), //색상 견본
-      home: MyCard(), //가장 먼저 화면에 보여지는 경로
+      home: MyHomePage(), //가장 먼저 화면에 보여지는 경로
     );
   }
 }
 
 //stateless stateful 모양이 변하는 요소가 한개라도 있다면 stateful widget 선택하기
 
-class MyCard extends StatelessWidget {
-  const MyCard({super.key});
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //무언가를 혼자해낼 수 있도록 발판을 만들어주는 것(빈 도화지 같은 역할)
       appBar: AppBar(
-        title: Text("Charactor card"),
-        centerTitle: true,
-        backgroundColor: Colors.white10,
-        elevation: 0.5, //padding 과 margin 의 차이점 : margin이 서로 다른 요소 간의 간격
+        title: Text("hello"),
       ),
       body: Center(
-        //항상 화면 정 중앙에 위치하는 것은 아님  //column 자체를 중앙으로
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, //column 세로 축 중에서 중앙에 위치
-          children: [Text("hi"), Text("hello"), Text("hawi")],
-        ),
-      ),
+          //자식 위젯을 가짐
+          child: Column(
+        //column이 자식을 가진다면 children(복수)
+        children: [Text("hello"), Text("My name is"), Text("meeju")],
+      )),
     );
   }
 }
